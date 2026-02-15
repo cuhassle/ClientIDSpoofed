e911a398717010f1253f5f6c42e054c39ebd9fbd
Novinity <novinity63@gmail.com>
2026-02-15 23:44:23 +1000

    client v1.0.0

---
+++ src/main/java/com/novinitygames/clientidClient/client/watcher/ResourcePackWatcher.java
@@ -0,0 +1,69 @@
+package com.novinitygames.clientidClient.client.watcher;
+
+import com.novinitygames.clientidClient.ClientidClient;
+import com.novinitygames.clientidClient.client.ClientidClientClient;
+import com.novinitygames.clientidClient.client.records.PackListC2SPayload;
+import com.novinitygames.clientidClient.client.util.ListerUtil;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.ResourcePackProfile;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResourcePackWatcher implements SimpleSynchronousResourceReloadListener {
+    private final Identifier id = Identifier.of(ClientidClient.MOD_ID, "resource_pack_watcher");
+    private List<String> lastEnabled = new ArrayList<>();
+
+    public ResourcePackWatcher() {
+        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(this);
+        updateLastEnabled();
+    }
+
+    @Override
+    public Identifier getFabricId() {
+        return id;
+    }
+
+    @Override
+    public void reload(ResourceManager manager) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client == null) return;
+
+        ArrayList<String> current = new ArrayList<>();
+        for (ResourcePackProfile profile : client.getResourcePackManager().getEnabledProfiles()) {
+            current.add(profile.getDisplayName().toString());
+        }
+
+        List<String> added = new ArrayList<>(current);
+        added.removeAll(lastEnabled);
+
+        List<String> removed = new ArrayList<>(lastEnabled);
+        removed.removeAll(current);
+
+        if (!added.isEmpty() || !removed.isEmpty()) {
+            if (ClientidClientClient.isConnectedToServer) {
+                String enabledPacks = String.join(",", ListerUtil.getEnabledPacks());
+                PackListC2SPayload payload = new PackListC2SPayload(enabledPacks);
+                ClientPlayNetworking.send(payload);
+            }
+        }
+
+        lastEnabled = current;
+    }
+
+    private void updateLastEnabled() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client == null) return;
+        ArrayList<String> list = new ArrayList<>();
+        for (ResourcePackProfile profile : client.getResourcePackManager().getEnabledProfiles()) {
+            list.add(profile.getDisplayName().toString());
+        }
+        lastEnabled = list;
+    }
+}
